ea5f318c8757b7caad96ee3af6c0d478fded4df9
zhangxunhui <zhangxunhui@nudt.edu.cn>
2021-08-13 23:19:20 +0800

    went through the comment process

---
+++ app/main.py
@@ -0,0 +1,29 @@
+import re, sys
+sys.path.append("./")
+import json5 as json
+from wsgiref.simple_server import make_server
+from app.services.comments import return_pr_decision
+from app.services.triggers import parseTriggers
+from app.models.trigger import Trigger
+
+def application(environ, start_response) -> bytearray:
+    start_response('200 OK', [('Content-Type', 'application/json')])
+    
+    request_body = environ["wsgi.input"].read(int(environ.get("CONTENT_LENGTH", 0)))
+    
+    json_str = request_body.decode('utf-8')
+    json_str = re.sub('\'','\"', json_str)
+    json_dict = json.loads(json_str)
+
+    trigger: Trigger = parseTriggers(json_dict)
+
+    return_pr_decision(trigger)
+
+    return ["success".encode('utf-8')]
+ 
+ 
+if __name__ == "__main__":
+    port = 4567
+    httpd = make_server("0.0.0.0", port , application)
+    print("serving http on port {0}...".format(str(port)))
+    httpd.serve_forever()
