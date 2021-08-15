5addaa914a67d0c25e6e4e9687a952d762c59d4a
zhangxunhui <zhangxunhui@nudt.edu.cn>
2021-08-15 14:52:58 +0800

    add func query_prs_4_scheduler

--- app/db/operators/pull_request_operator.py
+++ app/db/operators/pull_request_operator.py
@@ -1,28 +1,23 @@
-import sys, pathlib
-from turtle import update
-
-from sqlalchemy import MetaData, Table
+import sys, pathlib, asyncio
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[3]))
 
 from app.db.operators.base_operator import BaseOperator
 import app.db.tables.sqlalchemy_orm as tModel
 from app.models.pull_request import PullRequest
-from aiomysql import Connection
-from app.db.events import connect_to_db
-from app.utils.global_variables import GlobalVariable
 from app.models.pull_request import PullRequest
 from app.models.user import User
 from app.models.repository import Repository
-import asyncio
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import insert
+from sqlalchemy import insert, text
+from app.utils.config_loader import ConfigLoader
+from typing import List
 
 class PullRequestOperator(BaseOperator):
     def __init__(self) -> None:
         super().__init__()
 
 
-    async def insert_pull_request(self, pr: PullRequest):
+    async def insert_pull_request(self, pr: PullRequest) -> None:
         def run_queries(session):
             session.execute(insert(tModel.PullRequest).values(
                 owner_login = pr.owner.login,
@@ -31,24 +26,42 @@ class PullRequestOperator(BaseOperator):
                 state = pr.state,
                 locked = pr.locked,
                 created_at = pr.created_at,
-                updated_at = pr.updated_at
+                last_comment_at = pr.last_comment_at
             ))
         try:
             async with AsyncSession(self.engine) as session:
                 await session.run_sync(run_queries)
                 await session.commit()
-                print("pause")
         except Exception as e:
             print("error with func insert_pull_request: %s" % (repr(e)))
+
+    
+    async def query_prs_4_scheduler(self) -> List[PullRequest]:
+        try:
+            result: List[PullRequest] = []
+            env = ConfigLoader().load_env()
+            async with self.engine.connect() as connection:
+                q = '''
+                select pr.id, pr.owner_login, pr.repo_name, pr.number
+                from pull_requests pr
+                where last_comment_at < TIMESTAMPADD(HOUR, -%s, UTC_TIMESTAMP())
+                order by last_comment_at asc
+                ''' 
+                query_result = await connection.execute(text(q % env["SCHEDULER"]["CYCLE_HOUR"]))
+                for row in query_result:
+                    pr = PullRequest(owner=User(login=row['owner_login']), repo=Repository(name=row['repo_name']), number=row['number'], id=row['id'])
+                    result.append(pr)
+                return result
+        except Exception as e:
+            print("error with func query_prs_4_scheduler: %s" % (repr(e)))
 
 
 if __name__ == "__main__":
 
-    import pymysql
-    pymysql.install_as_MySQLdb() # we are using python3
     async def test_insert_pull_requests() -> None:
-        # await connect_to_db()
         prOp = PullRequestOperator()
-        await prOp.insert_pull_request(PullRequest(owner=User("test"), repo=Repository("test"), number=1, state="test", locked=1, created_at='2020-01-01 00:00:00', updated_at='2020-01-01 00:00:00'))
+        await prOp.query_prs_4_schedular()
+        # await prOp.insert_pull_request(PullRequest(owner=User("test"), repo=Repository("test"), number=1, state="open", locked=1, created_at='2020-01-01 00:00:00'))
 
-    asyncio.run(test_insert_pull_requests())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test_insert_pull_requests())
