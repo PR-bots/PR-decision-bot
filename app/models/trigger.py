6cb5a47c3b8b64a2adfdb20d4cafc35ce9d467e1
zhangxunhui <zhangxunhui@nudt.edu.cn>
2021-08-15 21:26:59 +0800

    change models due to the change of Trigger model and PullRequest model

--- app/models/trigger.py
+++ app/models/trigger.py
@@ -1,4 +1,5 @@
 import sys, pathlib
+from typing import Optional
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[2]))
 from app.models.pull_request import PullRequest
 from app.models.installation import Installation
@@ -7,21 +8,28 @@ from app.models.user import User
 
 class Trigger():
     installation: Installation
-    repo: Repository
-    sender: User
 
-    def __init__(self, installation, repo, sender) -> None:
+    def __init__(self, installation) -> None:
         self.installation = installation
-        self.repo = repo
-        self.sender = sender
 
 
 class PRTrigger(Trigger):
 
+    repo: Optional[Repository] = None
+    sender: Optional[User] = None
     pr: PullRequest
     action: str
 
     def __init__(self, installation, repo, sender, pr, action) -> None:
-        super(PRTrigger, self).__init__(installation, repo, sender)
+        super(PRTrigger, self).__init__(installation)
+        self.repo = repo
+        self.sender = sender
         self.pr = pr
         self.action = action
+
+class PRSchedulerTrigger(Trigger):
+    pr: PullRequest
+
+    def __init__(self, installation, pr) -> None:
+        super(PRSchedulerTrigger, self).__init__(installation)
+        self.pr = pr
