caa7ed93574b16e2a60aeb4dd631bdff05c37a4e
zhangxunhui <zhangxunhui@nudt.edu.cn>
2021-08-15 15:53:42 +0800

    change pull_requests

--- app/db/tables/sqlalchemy_orm.py
+++ app/db/tables/sqlalchemy_orm.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, VARCHAR, TIMESTAMP, Boolean, Text
+from sqlalchemy import Column, Integer, VARCHAR, TIMESTAMP, Boolean, Text, Index
 
 Base = declarative_base()
 
@@ -8,17 +8,19 @@ class PullRequest(Base):
     id = Column(Integer, primary_key=True)
     owner_login = Column(VARCHAR(255), nullable=False)
     repo_name = Column(VARCHAR(255), nullable=False)
+    installation_id = Column(Integer, nullable=False)
     number = Column(Integer, nullable=False)
-    state = Column(VARCHAR(255), nullable=False)
+    state = Column(VARCHAR(255), nullable=False, index=True)
+    locked = Column(Boolean, nullable=True, index=True)
     created_at = Column(TIMESTAMP, nullable=False)
-    updated_at = Column(TIMESTAMP, nullable=True)
-    comment_or_not = Column(Boolean, nullable=True)
-
+    last_comment_at = Column(TIMESTAMP, nullable=True) # store the time for the last comment
+    comment_or_not = Column(Boolean, nullable=False, default=0)
+Index("owner_repo_num", PullRequest.owner_login, PullRequest.repo_name, PullRequest.number, unique=True)
 
 class Comment(Base):
     __tablename__ = "comments"
     id = Column(Integer, primary_key=True)
-    pull_request_id = Column(Integer, nullable=False)
+    pull_request_id = Column(Integer, nullable=False, index=True)
     comment_id = Column(Integer, nullable=False)
     created_at = Column(TIMESTAMP, nullable=False)
     body = Column(Text, nullable=True)
