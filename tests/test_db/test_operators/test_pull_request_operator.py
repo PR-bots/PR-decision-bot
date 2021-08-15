34f2b1877dd938249fb19128741f0bd29f6af6c5
zhangxunhui <zhangxunhui@nudt.edu.cn>
2021-08-15 14:51:36 +0800

    add test for function query_prs_4_schedular

--- tests/test_db/test_operators/test_pull_request_operator.py
+++ tests/test_db/test_operators/test_pull_request_operator.py
@@ -11,3 +11,9 @@ from app.models.repository import Repository
 async def test_insert_pull_requests() -> None:
     prOp = PullRequestOperator()
     await prOp.insert_pull_request(PullRequest(owner=User("zxh2"), repo=Repository("test"), number=1, state="open", locked=1, created_at='2020-01-01 00:00:00'))
+
+@pytest.mark.asyncio
+async def test_query_prs_4_schedular() -> None:
+    prOp = PullRequestOperator()
+    result = await prOp.query_prs_4_schedular()
+    assert len(result) > 0
