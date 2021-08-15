a8df7408125d58cf8a3c0001caca12b0476be9ac
zhangxunhui <zhangxunhui@nudt.edu.cn>
2021-08-15 21:28:29 +0800

    change tests

--- tests/test_db/test_operators/test_pull_request_operator.py
+++ tests/test_db/test_operators/test_pull_request_operator.py
@@ -1,19 +1,26 @@
+from app.models.installation import Installation
+from gettext import install
 import sys, pathlib
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[3]))
 
 from app.db.operators.pull_request_operator import PullRequestOperator
 import pytest
 from app.models.pull_request import PullRequest
+from app.models.installation import Installation
 from app.models.user import User
 from app.models.repository import Repository
 
 @pytest.mark.asyncio
 async def test_insert_pull_requests() -> None:
     prOp = PullRequestOperator()
-    await prOp.insert_pull_request(PullRequest(owner=User("zxh2"), repo=Repository("test"), number=1, state="open", locked=1, created_at='2020-01-01 00:00:00'))
+    installation=Installation(id=1, app_id=None)
+    assert installation != None
+    pr=PullRequest(owner=User("zxh2"), repo=Repository("test"), number=1, state="open", locked=1, created_at='2020-01-01 00:00:00')
+    assert pr != None
+    await prOp.insert_pull_request(pr=pr, installation=installation)
 
 @pytest.mark.asyncio
-async def test_query_prs_4_scheduler() -> None:
+async def test_query_prScheduler_4_scheduler() -> None:
     prOp = PullRequestOperator()
-    result = await prOp.query_prs_4_scheduler()
-    assert len(result) > 0
+    result = await prOp.query_prScheduler_4_scheduler()
+    assert len(result) == 0
